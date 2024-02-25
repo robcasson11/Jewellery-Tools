003f93412d6671a301df4a37c50b2842d60fe315
rob <robertcasson@gmail.com>
2024-02-25 22:34:41 +0000

    FEAT: Size converter built in to App.jsx and fully functioning. BUG: Dropdowns should reset when other dropdowns change. TODO: Abstract the size converter into it's own components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,70 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import "./App.css";
 import WeightInput from "./components/WeightInput";
+import sizeConverter from "./functions/sizeConverter";
+import sizeConversions from "./data/sizeConversionsData";
 
 function App() {
-  const [values, setValues] = useState({
+  const [weightValues, setWeightValues] = useState({
     gold: "",
     plat: "",
     sil: "",
     wax: "",
   });
 
+  const [sizeValues, setSizeValues] = useState();
+
+  const handleSizeConversion = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    setSizeValues(sizeConverter(name, value));
+    console.log(event);
+  };
+
   return (
-    <fieldset>
-      <legend>Type in your value to the relevant input.</legend>
-      {Object.entries(values).map(([type, value]) => (
-        <WeightInput
-          key={type}
-          type={type}
-          value={value}
-          values={values}
-          setValues={setValues}
-        />
-      ))}
-    </fieldset>
+    <>
+      <fieldset>
+        <legend>Type in your value to the relevant input.</legend>
+        {Object.entries(weightValues).map(([type, value]) => (
+          <WeightInput
+            key={type}
+            type={type}
+            value={value}
+            values={weightValues}
+            setValues={setWeightValues}
+          />
+        ))}
+      </fieldset>
+      <fieldset>
+        <legend>Pick your ring size</legend>
+        {sizeConversions.map((type) => {
+          return (
+            <Fragment key={type.name}>
+              <label htmlFor="type">{type.name}</label>
+              <br />
+              <select
+                id={type.name}
+                name={type.name}
+                onChange={(e) => handleSizeConversion(e)}
+              >
+                <option value={type.data[sizeValues]}>
+                  {type.data[sizeValues]}
+                </option>
+
+                {type.data.map((value) => {
+                  return (
+                    <option key={value} value={value}>
+                      {value}
+                    </option>
+                  );
+                })}
+              </select>
+              <br />
+            </Fragment>
+          );
+        })}
+      </fieldset>
+    </>
   );
 }
 
