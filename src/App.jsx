e0fef02823ce79622893c25991c7b1567cf51e41
rob <robertcasson@gmail.com>
2024-03-06 20:46:03 +0000

    BUGFIX: Broken nav link repaired

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
             <h3 className="float-md-start mb-2">Jewellery Tools</h3>
             <nav className="nav nav-masthead justify-content-center float-md-end">
               <Link
-                to={"/"}
+                to={"/jewellery-tools"}
                 className={`nav-link fw-bold py-1 px-0 ${
-                  location.pathname === "/" ? "active" : null
+                  location.pathname === "/jewellery-tools" ? "active" : null
                 }`}
               >
                 Home
