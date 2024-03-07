a5776fc86d2ed73a289a61e91811b020ca914ac9
rob <robertcasson@gmail.com>
2024-03-07 20:49:50 +0000

    BUGFIX: Previouse chore updated to work with functions

--- src/data/weightConversionsData.js
+++ src/data/weightConversionsData.js
@@ -1,23 +1,23 @@
 const weightConversions = {
   gold: {
-    plat: 1.66,
-    sil: 0.9,
+    platinum: 1.66,
+    silver: 0.9,
     wax: 0.071,
   },
   platinum: {
     gold: 0.6,
-    sil: 0.48,
+    silver: 0.48,
     wax: 0.049,
   },
   silver: {
     gold: 1.11,
-    plat: 2.06,
+    platinum: 2.06,
     wax: 0.097,
   },
   wax: {
     gold: 14.1,
-    plat: 20.6,
-    sil: 10.3,
+    platinum: 20.6,
+    silver: 10.3,
   },
 };
 
