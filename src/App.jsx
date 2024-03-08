fef7a0b9aa13f3fac91e87f4b30075664129acc1
rob <robertcasson@gmail.com>
2024-03-08 07:01:31 +0000

    BUGFIX: Attempt to correct routing

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
             <h3 className="float-md-start mb-2">Jewellery Tools</h3>
             <nav className="nav nav-masthead justify-content-center float-md-end">
               <Link
-                to={"jewellery-tools"}
+                to={"/"}
                 className={`nav-link fw-bold py-1 px-0 ${
-                  location.pathname === "/jewellery-tools" ? "active" : null
+                  location.pathname === "/" ? "active" : null
                 }`}
               >
                 Home
